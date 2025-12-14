d629a1bb00958952872f56fde1d1fde4361cf28d
gaitano <dev.gaitano@gmail.com>
2025-12-14 19:41:05 +0300

    fix: updated default port from 5000 to 5001 in frontendApi.py

--- frontendApi.py
+++ frontendApi.py
@@ -92,5 +92,5 @@ def shipments():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=False)
