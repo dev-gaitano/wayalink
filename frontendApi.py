3ede3b5a6307a3c6d61816ef233abad6e264a6f4
gaitano <dev.gaitano@gmail.com>
2026-02-10 10:43:39 +0300

    feat: added validation for required fields in signup process and streamline company insertion logic

--- frontendApi.py
+++ frontendApi.py
@@ -50,6 +50,10 @@ def signup() -> Response:
         signup_password: str | None = signup_data.get("signupPassword")
         confirmed_password: str | None = signup_data.get("confirmedPassword")
 
+        if not all([gender, firstname, lastname, id_number, phone_number,
+                    signup_email, company_name, signup_password, confirmed_password]):
+            return jsonify({ "success" : False, "message" : "All input fields required" })
+
         # Process user signup data
         def process_signup(signup_password, confirmed_password) -> Response:
             if signup_password == confirmed_password:
@@ -64,21 +68,19 @@ def signup() -> Response:
 
                 if not user:
                     # Insert company ID 
-                    cursor.execute("""
-                                   INSERT INTO companies (name) VALUES (%s)
-                                   """, (company_name,))
-                    conn.commit()
-
                     # Select ID where company_name in comapnies matches user input
                     cursor.execute("""
-                                   SELECT id FROM companies WHERE name = %s
+                                   INSERT INTO companies (name) VALUES (%s)
+                                   RETURNING id
                                    """, (company_name,))
 
                     # Store ID in a variable
-                    signup_user_company_id: tuple | None = cursor.fetchone()
+                    company_id_result: tuple | None = cursor.fetchone()
 
-                    if not signup_user_company_id:
+                    if not company_id_result:
                         return jsonify({"success": False, "message": "Failed to create company"})
+
+                    signup_user_company_id = company_id_result[0]
 
                     # Add variable value to users company_id column
                     cursor.execute("""
@@ -168,7 +170,7 @@ def login() -> Response:
                 # Verify if is_admin
                 if role == "Admin":
                     cursor.execute("UPDATE users SET last_login = NOW() WHERE id = %s", (user_id,))
-                    conn.commit
+                    conn.commit()
 
                     return jsonify({"success": True, "message": "Login successful"})
                 else:
